4b8ad6e119c68c6072935a4f7dd142edb2d93238
bia <euannebeatrizcm@gmail.com>
2023-08-10 14:04:42 -0300

    numbergenerate

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('generate');
+
+button.addEventListener('click', function() {
+    const min = parseInt(document.getElementById('min').value);
+    const max = parseInt(document.getElementById('max').value);
+
+    let result = Math.floor(Math.random() * (max - min + 1)) + min
+
+    document.querySelector('#result > span ').textContent = result;
+
+});
